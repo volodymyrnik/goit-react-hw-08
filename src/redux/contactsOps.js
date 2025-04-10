dd761d59a202e95575c4d6522e59cf9cf48e6983
Volodymyr Nikiforenko <vl.nikiforenko@gmail.com>
2025-04-10 22:40:49 +0300

    try 2

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://67f79ca42466325443e9f35e.mockapi.io";
+axios.defaults.baseURL = "https://67ee543ec11d5ff4bf792e14.mockapi.io";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchContacts",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
-      return response.data;
+      const responce = await axios.get("/contacts");
+      return responce.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -19,8 +19,8 @@ export const addContacts = createAsyncThunk(
   "contacts/addContacts",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post("/contacts", newContact);
-      return response.data;
+      const responce = await axios.post("/contacts", newContact);
+      return responce.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -30,8 +30,8 @@ export const deleteContacts = createAsyncThunk(
   "contacts/deleteContacts",
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${contactId}`);
-      return response.data;
+      const responce = await axios.delete(`/contacts/${contactId}`);
+      return responce.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,47 +1,100 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { initialStateContacts } from "./constants";
-import { addContact, deleteContact, fetchContacts } from "./contactsOps";
+import { createSelector, createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContacts, deleteContacts } from "./contactsOps";
+import toast from "react-hot-toast";
+import { selectNameFilter } from "./filtersSlice";
 
-const handleRejected = (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-}
+export const selectContacts = (state) => state.contacts.items;
+export const selectLoading = (state) => state.contacts.loading;
+export const selectError = (state) => state.contacts.error;
+
+// export const selectVisibleContacts = (state) => {
+//   const contacts = selectContacts(state);
+//   const filter = selectNameFilter(state);
+//   return contacts.filter((contact) =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+// };
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 
 const contactsSlice = createSlice({
-    name: 'contacts',
-    initialState: initialStateContacts,
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchContacts.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(fetchContacts.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.error = null;
-                state.items = action.payload;
-            })
-            .addCase(fetchContacts.rejected, handleRejected)
-            .addCase(addContact.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(addContact.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.error = null;
-                state.items.push(action.payload);
-            })
-            .addCase(addContact.rejected, handleRejected)
-            .addCase(deleteContact.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(deleteContact.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.error = null;
-                const index = state.items.findIndex(contact => contact.id === action.payload.id);
-                state.items.splice(index, 1);
-            })
-            .addCase(deleteContact.rejected, handleRejected)
-    }
-})
+  name: "contacts",
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+    message: null,
+  },
 
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(addContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items.push(action.payload);
+        toast("Contact is added!", {
+          icon: "👏",
+          style: {
+            fontWeight: "bold",
+            borderRadius: "10px",
+            background: "#fafafa",
+            color: "#108606",
+          },
+        });
+      })
+      .addCase(addContacts.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(deleteContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
 
-export const contactsReducer = contactsSlice.reducer;
+        toast.error("Contact is deleted!", {
+          style: {
+            fontWeight: "bold",
+            borderRadius: "10px",
+            background: "#fafafa",
+            color: "#c40707",
+          },
+        });
+      })
+      .addCase(deleteContacts.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      });
+  },
+});
+
+export default contactsSlice.reducer;
