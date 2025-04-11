99f46246e6b1c096845e339ef91155a2f0814ecd
Volodymyr Nikiforenko <vl.nikiforenko@gmail.com>
2025-04-11 22:51:02 +0300

    One more try

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,42 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
-import { fetchContacts } from '../../redux/contacts/operations'; // Если есть операция для получения контактов
 import { selectFilteredContacts } from '../../redux/contacts/selectors';
-import { selectAllContacts } from "../../redux/contacts/selectors.js";
-import { selectFilter } from '../../redux/filters/selectors.js';
 import css from './ContactList.module.css';
 
 const ContactList = () => {
-  const dispatch = useDispatch();
-
-  const visibleContacts = useSelector(selectFilteredContacts) || []; // Селектор для отфильтрованных контактов
-  const contacts = useSelector((state) => state.contacts.items); // Здесь предполагаем, что контакты сохраняются в state.contacts.items
-  // const contacts = useSelector(selectAllContacts); // Все контакты
-  const filter = useSelector((state) => state.filter.filter); // Допустим, фильтр в state.filter.filter
-  // const filter = useSelector(selectFilter) || ""; // Значение фильтра
-
-  console.log('visibleContacts:', visibleContacts);
-  console.log('contacts:', contacts);
-  console.log('filter:', filter);
-
-
-  useEffect(() => {
-    console.log('Contacts:', contacts); // Логируем контакты
-    console.log('Filtered Contacts:', visibleContacts); // Логируем отфильтрованные контакты
-  }, [contacts, visibleContacts]);
-
-  // Если filter не пустой, фильтруем контакты по имени или номеру
-  const filteredContacts = contacts.filter(contact =>
-    (contact?.name?.toLowerCase()?.includes(filter.toLowerCase()) || contact?.number?.includes(filter))
-  );
-
-  console.log('filteredContacts:', filteredContacts);
+  const visibleContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css['contact-list']}>
-      {filteredContacts.length > 0 ? (
-        filteredContacts.map(({ id, name, number }) =>
+      {visibleContacts.length > 0 ? (
+        visibleContacts.map(({ id, name, number }) =>
           id && name && number ? (
             <Contact key={id} id={id} name={name} number={number} />
           ) : null
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,6 +2,7 @@ export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
+import { createSelector } from '@reduxjs/toolkit';
 
 
 
